124e815cb642b269017a18517e30908091167c75
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-04-09 14:07:14 +0100

    Adding new file for functions
    Co-Authored-By: Lewis <lewisknewton@users.noreply.github.com>
    Co-Authored-By: \x53\x6f\x75\x6c\x4a\x61\x68 <soul-jah@users.noreply.github.com>

---
+++ data_parsing.py
@@ -0,0 +1,29 @@
+# Land
+
+def land():
+
+# Disembark
+
+def disembark():
+
+# NewPassengers
+# Refuel
+# Depart
+class Plane:
+    def __init__(self, fuel, capacity, passengers, highPriority):
+        this.fuel = fuel
+        this.capacity = capacity
+        this.passengers = passengers
+        this.highPriorty = highPriority
+        this.waitTime = 0.0
+
+def main():
+    LF333 = Plane(11.0, 20.0, 24, false)
+    LF233 = Plane(9.0, 21.0, 2, false)
+    LF133 = Plane(7.0, 23.0, 12, false)
+    BB456 = Plane(310.0, 400.0, 234, true)
+    HL666 = Plane(510.0, 550, 275, true)
+    
+    planeList = [LF333, LF233, BB456, HL666]
+    
+    priorityScheduling(planeList)
