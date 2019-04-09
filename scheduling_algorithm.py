05ef9bb6653a15c61bca0fc91189a4e8c36fbc8d
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-04-09 12:59:28 +0100

    initialising first files to repo

---
+++ scheduling_algorithm.py
@@ -0,0 +1,18 @@
+class Plane:
+    def __init__(self, fuel, capacity, highPriority):
+        this.fuel = fuel
+        this.capacity = capacity
+        this.highPriorty = highPriority
+
+
+def main():
+    LF333 = Plane(11.0, 20.0, false)
+    LF233 = Plane(9.0, 21.0, false)
+    LF133 = Plane(7.0, 23.0, false)
+    BB456 = Plane(310.0, 400.0, true)
+    HL666 = Plane(510.0, 550, true)
+    
+    planeList = [LF333, LF233, BB456, HL666]
+    
+    
+def
