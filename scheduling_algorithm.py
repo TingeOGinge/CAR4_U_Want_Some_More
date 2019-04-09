b20e87a5931e127783cdaad2d84b0f7593fe4952
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-04-09 14:02:53 +0100

    Added missing passengers variables to Plane class
    Co-Authored-By: Lewis <lewisknewton@users.noreply.github.com>
    Co-Authored-By: \x53\x6f\x75\x6c\x4a\x61\x68 <soul-jah@users.noreply.github.com>

--- scheduling_algorithm.py
+++ scheduling_algorithm.py
@@ -1,33 +1,41 @@
 class Plane:
-    def __init__(self, fuel, capacity, highPriority):
+    def __init__(self, fuel, capacity, passengers, highPriority):
         this.fuel = fuel
         this.capacity = capacity
+        this.passengers = passengers
         this.highPriorty = highPriority
-
+        this.waitTime = 0.0
 
 def main():
-    LF333 = Plane(11.0, 20.0, false)
-    LF233 = Plane(9.0, 21.0, false)
-    LF133 = Plane(7.0, 23.0, false)
-    BB456 = Plane(310.0, 400.0, true)
-    HL666 = Plane(510.0, 550, true)
+    LF333 = Plane(11.0, 20.0, 24, false)
+    LF233 = Plane(9.0, 21.0, 2, false)
+    LF133 = Plane(7.0, 23.0, 12, false)
+    BB456 = Plane(310.0, 400.0, 234, true)
+    HL666 = Plane(510.0, 550, 275, true)
     
     planeList = [LF333, LF233, BB456, HL666]
     
+    priorityScheduling(planeList)
+    
     
 def priorityScheduling(planeList):
-    pass
+    planeList = sorted(planeList, key = lambda planeList:planeList[2])
+    print(String(planeList))
+    for plane in planeList:
+        findWaitTime(plane)
+        findTurnAroundTime(plane)
+    findAvgTime(planeList)
         
 
-def findWaitTime():
+def findWaitTime(plane):
     pass
 
 
-def findTurnAroundTime():
+def findTurnAroundTime(plane):
     pass
 
 
-def findAvgTime():
+def findAvgTime(planeList):
     pass
 
 
