cf5c2905792dbb6f07c2f3f74d0c4f0612b9f35e
soul-jah <souljah@tutanota.com>
2019-04-10 18:50:55 +0900

    Changes to Data Parsing, Auto generation and file write out.

--- data_parsing.py
+++ data_parsing.py
@@ -1,42 +1,27 @@
-class Plane:
-    def __init__(self, fuel, capacity, passengers, highPriority):
-        self.fuel = fuel
-        self.capacity = capacity
-        self.passengers = passengers
-        self.inAndOutTime = self.passengers * 2
-        self.highPriorty = highPriority
-        self.waitTime = 0.0
+import random
+from random import randint
+import string
 
-        # Land
-        def land(self):           
-            if self.highPriorty:
-                return 1
-            else
-                return 2
 
-        # # Refuel
-        # 
-        # def refuel(self):
-        #     pass
+# Random data generation for larger planes
+def randBigPlane():
+    randID = ''.join(random.choice(string.ascii_uppercase) for i in range(2)) + str(randint(100,999))
+    fuel = randint(300, 550)
+    capacity = fuel + randint(10, 100)
+    passengers = randint(200, 300)
+    return(randID, fuel, capacity, passengers, True)
 
-        #One ladder for priority shared if not in use
-        
 
-        #One ladder dedicated to small aircraft 
-        #FCFS
-                
-        # Depart
-        def depart(self):
-            return 1
+# Random data generation for smaller planes
+def randSmallPlane():
+    randID = ''.join(random.choice(string.ascii_uppercase) for i in range(2)) + str(randint(100,999))
+    fuel = randint(5, 15)
+    capacity = fuel + randint(5, 15)
+    passengers = randint(2, 25)
+    return(randID, fuel, capacity, passengers, True)
 
-def main():
-    #small planes average 12.3 fuelling requirements
-    LF333 = Plane(11.0, 20.0, 24, False)
-    LF233 = Plane(9.0, 21.0, 2, False)
-    LF133 = Plane(7.0, 23.0, 12, False)
-    #large planes average 65
-    BB456 = Plane(310.0, 400.0, 234, True)
-    HL666 = Plane(510.0, 550, 275, True) 
-    
-    planeList = [LF333, LF233, BB456, HL666]
-    
+def fileWrite():
+    output = str(randSmallPlane())
+    file = open("test.txt","w")
+    file.write(output)
+    file.close()
